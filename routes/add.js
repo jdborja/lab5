abbb75acdbebb41f7962f4d82f62bdc8a4b31075
Jeremy <jdborja@ucsd.edu>
2018-02-08 10:25:11 -0800

    done

--- routes/add.js
+++ routes/add.js
@@ -2,4 +2,9 @@ var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
 	// Your code goes here
+	var newFriend = {
+		"name": req.query.name,
+		"description": req.quuery.description,
+		"imageURL": 'http://lorempixel.com/400/400/people'
+	};
  }
